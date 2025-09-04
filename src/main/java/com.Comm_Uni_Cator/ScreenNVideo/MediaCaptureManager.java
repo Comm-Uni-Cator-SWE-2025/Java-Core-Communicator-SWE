8fbe30510cb9cff90a359564dde884f60092e09f
alonot <priyanshup27092004@gmail.com>
2025-09-04 18:54:29 +0530

    Project Structure init

---
+++ src/main/java/com.Comm_Uni_Cator/ScreenNVideo/MediaCaptureManager.java
@@ -0,0 +1,10 @@
+package com.Comm_Uni_Cator.ScreenNVideo;
+
+/**
+ * Hello world!
+ */
+public class MediaCaptureManager {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
