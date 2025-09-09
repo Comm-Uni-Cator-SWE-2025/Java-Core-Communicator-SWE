099d749b744e7f622225f02f002a37da59770787
LOGANATH07 <112201016@smail.iitpkd.ac.in>
2025-09-09 08:05:44 +0530

    Initial Networking base for prototyping...

---
+++ Networking/src/main/java/AbstractController.java
@@ -0,0 +1,9 @@
+/**
+ * The addUser function here invokes a function on topology which in turn
+ * adds the client to a cluster
+ * */
+
+public interface AbstractController {
+
+    void addUser(String ip, Integer port);
+}
---
+++ Networking/src/main/java/Topology.java
@@ -0,0 +1,24 @@
+import java.util.List;
+
+/**
+ * The main architecture of the networking module.
+ * Implement the interface abstractController and its functions
+ */
+public class Topology {
+    /*
+     TODO: Create and remove clusters as required
+     TODO: Add and move clients from one cluster to another
+    */
+    /**
+     * The number of clusters in the current network.
+     *
+     */
+    private final int _numClusters = 0;
+    private List<Integer> _numClientsPerCluster;
+
+    public Topology() {
+
+    }
+
+
+}
