97e4ca065e859c815b7d9f5b3548fe6e6a74d680
alonot <priyanshup27092004@gmail.com>
2025-09-10 19:00:23 +0530

    MediaManager basic interface with networking and RPC

--- src/main/java/com.Comm_Uni_Cator/Networking/AbstractNetworking.java
+++ src/main/java/com.Comm_Uni_Cator/Networking/AbstractNetworking.java
@@ -1,6 +1,6 @@
 package com.Comm_Uni_Cator.Networking;
 
-public interface abstractNetworking {
+public interface AbstractNetworking {
     void SendData(byte[] data, String[] dest,int[] port);
     void Subscribe(String name, MessageListener function);
     void RemoveSubscription(String name);
---
+++ src/main/java/com.Comm_Uni_Cator/RPC/RProcedure.java
@@ -0,0 +1,5 @@
+package com.Comm_Uni_Cator.RPC;
+
+public interface RProcedure {
+    byte[] call(byte[] args);
+}
