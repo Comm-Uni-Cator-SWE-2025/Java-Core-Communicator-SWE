89035000ec203c9765ff8768ba014bf84321c1c3
aman112201041 <112201041@smail.iitpkd.ac.in>
2025-09-10 08:11:50 +0530

    added Hasher

--- src/main/java/com.Comm_Uni_Cator/ScreenNVideo/IHasher.java
+++ src/main/java/com.Comm_Uni_Cator/ScreenNVideo/IHasher.java
@@ -3,7 +3,7 @@ package com.Comm_Uni_Cator.ScreenNVideo;
 public interface IHasher {
     /**
      * Computes a hash for an image patch/tile
-     * @param img->pixel data (int[w][h][3]); (x, y)->location of the first corner; (w, h)->height and width
+     * @param img->pixel data (int[][][]); (x, y)->location of the first corner; (w, h)->height and width
      * @return hash as a long
      */
     long hash(int[][][] img, int x, int y, int w, int h);
