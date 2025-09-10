140d46c24cfb5a39a669ea400f888c4edce68200
hemant030406 <112201024@smail.iitpkd.ac.in>
2025-09-10 22:49:04 +0530

    implement ClientServer's sendTo function

--- Networking/src/main/java/ClientServer.java
+++ Networking/src/main/java/ClientServer.java
@@ -1,5 +1,10 @@
+import java.net.DatagramSocket;
+import java.net.DatagramPacket;
+import java.io.IOException;
+import java.net.InetAddress;
+
 /**
- * Implement the following
+ * Implement the following.
  * > a send function from client and server perspective
  * > a receive function from the server
  * > The server send function can use the interface abstractTopology
@@ -7,4 +12,90 @@
  * */
 
 public class ClientServer {
+    /**
+     * A variable for: Is this client a server?.
+     */
+    private boolean isServer;
+
+    /**
+     * Socket reserver for this client/server.
+     */
+    private DatagramSocket socket;
+
+    // open a udp server socket for this client
+    public  ClientServer() {
+        this.isServer = false;
+        try {
+            this.socket = new DatagramSocket();
+        } catch (IOException e) {
+            System.err.println("Client error: " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
+
+    // TODO: wait for routing packet from the server
+
+    /**
+     * Topology server will implement this func.
+     * @param dest Destination IP.
+     */
+    boolean destInCluster(final String dest) {
+        return true;
+    }
+
+    /**
+     * Function to send data to dest:port.
+     * @param data Data to be sent across
+     * @param dest Destination IP.
+     * @param port Destination PORT.
+     */
+    public void sendTo(final byte[] data, final String dest, final Integer port) {
+        if (this.isServer) {
+            if (destInCluster(dest)) {
+                try {
+                    final DatagramPacket sendPacket = new DatagramPacket(data, data.length, InetAddress.getByName(dest), port);
+                    try {
+                        this.socket.send(sendPacket);
+                    }  catch (IOException e) {
+                        System.err.println("Client error: " + e.getMessage());
+                        e.printStackTrace();
+                    }
+                }  catch (IOException e) {
+                    System.err.println("Client error: " + e.getMessage());
+                    e.printStackTrace();
+                }
+            } else {
+                try {
+                    final DatagramPacket sendPacket = new DatagramPacket(data, data.length, getServer(dest), port); // TODO: getServer to be implemented by Topology.
+                    try {
+                        this.socket.send(sendPacket);
+                    }  catch (IOException e) {
+                        System.err.println("Client error: " + e.getMessage());
+                        e.printStackTrace();
+                    }
+                }  catch (IOException e) {
+                    System.err.println("Client error: " + e.getMessage());
+                    e.printStackTrace();
+                }
+            }
+        } else {
+            if (destInCluster(dest)) {
+                try {
+                    final DatagramPacket sendPacket = new DatagramPacket(data, data.length, InetAddress.getByName(dest), port);
+                    try {
+                        this.socket.send(sendPacket);
+                    }  catch (IOException e) {
+                        System.err.println("Client error: " + e.getMessage());
+                        e.printStackTrace();
+                    }
+                }  catch (IOException e) {
+                    System.err.println("Client error: " + e.getMessage());
+                    e.printStackTrace();
+                }
+            } else {
+                // TODO: get server ip from routing packet.
+                // send(data, clusterServerIP, port);
+            }
+        }
+    }
 }
