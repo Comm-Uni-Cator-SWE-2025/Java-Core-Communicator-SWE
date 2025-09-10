7bf2f03bd11874c5384b5e18dbd8df85c81a57b8
LOGANATH07 <112201016@smail.iitpkd.ac.in>
2025-09-10 20:02:45 +0530

    Added getServer function

--- Networking/src/main/java/Topology.java
+++ Networking/src/main/java/Topology.java
@@ -1,10 +1,12 @@
-import java.util.List;
+import java.util.ArrayList;
 
+
+record ClientNode(String ipAddress, int port){}
 /**
  * The main architecture of the networking module.
  * Implement the interface abstractController and its functions
  */
-public class Topology {
+public class Topology implements AbstractTopology, AbstractController {
     /*
      TODO: Create and remove clusters as required
      TODO: Add and move clients from one cluster to another
@@ -13,12 +15,33 @@ public class Topology {
      * The number of clusters in the current network.
      *
      */
-    private final int _numClusters = 0;
-    private List<Integer> _numClientsPerCluster;
+    private ArrayList<ArrayList<String>> _destinationIP;
+    private ArrayList<ArrayList<Integer>> _destinationPort;
+    private ArrayList<ClientNode> _clusters;
+    private int _numClusters = 0;
+    private int _numClients = 0;
+
 
     public Topology() {
 
     }
 
+    @Override
+    public ClientNode GetServer(String dest) {
+        for (int i = 0; i < _destinationIP.size(); i++) {
+            ArrayList<String> clusterClients = _destinationIP.get(i);
+            int idx = clusterClients.indexOf(dest);
+            if(idx == -1){
+                // Debug write the destination not found
+                System.out.println("Destination ip address not found");
+            }
+            return _clusters.get(idx);
+        }
+        return null;
+    }
 
+    @Override
+    public void addUser(String ip, Integer port) {
+        _numClients += 1;
+    }
 }
