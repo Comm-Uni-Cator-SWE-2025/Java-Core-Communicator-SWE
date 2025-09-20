9773b730fb0ce921b3103b27c6faf53c28d80abf
Devansh Kesan <devanshkesan101@gmail.com>
2025-09-21 04:56:00 +0530

    Updated and refactored Stitchable

--- java/module-screen-video/src/main/java/com/swe/ScreenNVideo/PatchGenerator/Stitchable.java
+++ java/module-screen-video/src/main/java/com/swe/ScreenNVideo/PatchGenerator/Stitchable.java
@@ -2,5 +2,9 @@ package com.swe.ScreenNVideo.PatchGenerator;
 
 // Interface
 public interface Stitchable {
-    void applyOn(int[][][] canvas);
+    void applyOn(int[][] canvas);
+    int getHeight();
+    int getWidth();
+    int getX();
+    int getY();
 }
