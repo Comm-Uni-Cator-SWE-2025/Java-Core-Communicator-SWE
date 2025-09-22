70435c3590958d368c8feacf3fa8aa6322e061f8
andhkr <kanup3151@gmail.com>
2025-09-22 22:30:19 +0530

    JPEG Codec out of bounds fix

--- java/module-screen-video/src/main/java/com/swe/ScreenNVideo/Codec/JpegCodec.java
+++ java/module-screen-video/src/main/java/com/swe/ScreenNVideo/Codec/JpegCodec.java
@@ -1,6 +1,7 @@
 package com.swe.ScreenNVideo.Codec;
 
 import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
 
 public class JpegCodec implements Codec {
     private int[][] screenshot;
@@ -19,21 +20,26 @@ public class JpegCodec implements Codec {
 
     //convert screenshot into YCbCr format (4:2:0) chroma sampling
     @Override
-    public byte[] encode(int x, int y, int height, int width) {
+    public byte[] encode(int topLeftX, int topLeftY, int height, int width) {
 
-        System.out.println("x y W H = " + x + " " + y + ":" + width + " " + height);
+//        System.out.println("topLeftX topLeftY W H = " + topLeftX + " " + topLeftY + ":" + width + " " + height);
+
+        if (height % 2 == 1 || width % 2 == 1) {
+            throw new RuntimeException("Invalid Matrix for encoding");
+        }
 
         int[][] YMatrix = new int[height][width];
         int[][] CbMatrix = new int[height / 2][width / 2];
         int[][] CrMatrix = new int[height / 2][width / 2];
 
-
-        for (int i = y; i - y < height; i += 2) {
-            for (int j = x; j - x < width; j += 2) {
+        final int bottomLeftX = topLeftX + width;
+        final int bottomLeftY = topLeftY + height;
+        for (int i = topLeftY; i < bottomLeftY; i += 2) {
+            for (int j = topLeftX; j < bottomLeftX; j += 2) {
                 double cb_pixel = 0;
                 double cr_pixel = 0;
-                for (int ii = i; ii - i < 2; ++ii) {
-                    for (int jj = j; jj - j < 2; ++jj) {
+                for (int ii = i; ii < i + 2; ++ii) {
+                    for (int jj = j; jj < j + 2; ++jj) {
                         int pixel = screenshot[ii][jj];
 
                         int r = (pixel >> 16) & 0xff;
@@ -42,27 +48,53 @@ public class JpegCodec implements Codec {
 
                         // conversion to YCbCr
                         int Y = (int) (0.299 * r + 0.587 * g + 0.114 * b);
-                        double Cb = 128 - 0.168736 * r - 0.331264 * g + 0.5 * b;
-                        double Cr = 128 + 0.5 * r - 0.418688 * g - 0.081312 * b;
+                        final double Cb = 128 - 0.168736 * r - 0.331264 * g + 0.5 * b;
+                        final double Cr = 128 + 0.5 * r - 0.418688 * g - 0.081312 * b;
 
                         // Clamp to 0-255
                         Y = Math.min(255, Math.max(0, Y));
-                        YMatrix[i + ii][j + jj] = Y;
+                        YMatrix[ii - topLeftY][jj - topLeftX] = Y;
                         cb_pixel += Cb;
                         cr_pixel += Cr;
                     }
                 }
 
-                CbMatrix[i / 2][j / 2] = Math.min(255, Math.max(0, (int) (cb_pixel / 4)));
-                CrMatrix[i / 2][j / 2] = Math.min(255, Math.max(0, (int) (cr_pixel / 4)));
+                final int posY = i - topLeftY;
+                final int posX = j - topLeftX;
+                CbMatrix[posY / 2][posX / 2] = Math.min(255, Math.max(0, (int) (cb_pixel / 4)));
+                CrMatrix[posY / 2][posX / 2] = Math.min(255, Math.max(0, (int) (cr_pixel / 4)));
 
             }
         }
 
-        String str = "H:" + height + ",W:" + width + ";" +
-            "Y:" + zigZagScan(YMatrix) + "Cb:" + zigZagScan(CbMatrix) +
-            "Cr:" + zigZagScan(CrMatrix);
-        byte[] imagebytes = str.getBytes(StandardCharsets.UTF_8);
+        String str = "H:" + height + ",W:" + width + ";"
+            + "Y:" + zigZagScan(YMatrix) + "Cb:" + zigZagScan(CbMatrix)
+            + "Cr:" + zigZagScan(CrMatrix);
+        final byte[] imagebytes = str.getBytes(StandardCharsets.UTF_8);
+
+        int[][] decodedImage = decode(imagebytes);
+
+        for (int i = 0; i < width; i++) {
+            for (int j = 0; j < height; j++) {
+                if (decodedImage[j][i] != screenshot[j + topLeftY][i + topLeftX]) {
+                    for (int jj = 0; jj < 1; jj++) {
+                        for (int ii = 0; ii < width; ii++) {
+                            System.out.printf("%02x ", decodedImage[jj][ii]);
+
+                        }
+                        System.out.println();
+                    }
+                    for (int jj = 0; jj < 1; jj++) {
+                        for (int ii = 0; ii < width; ii++) {
+                            System.out.printf("%02x ", screenshot[jj + topLeftY][ii + topLeftX]);
+                        }
+                        System.out.println();
+                    }
+                    throw new RuntimeException("Mis Match");
+                }
+            }
+        }
+
         return imagebytes;
     }
 
@@ -79,11 +111,13 @@ public class JpegCodec implements Codec {
 
         // Extracting Y,Cb,Cr from the string
         String Ystring = parts[1].split("Cb:")[0].split("Y:")[1];
-        String Cbstring = parts[1].split("Cr:")[0].split("Cb")[1];
+        String Cbstring = parts[1].split("Cr:")[0].split("Cb:")[1];
         String Crstring = parts[1].split("Cr:")[1];
 
         int[][] Y = reverseZigZagScan(height, width, Ystring);
+//        System.out.println("Done" + Cbstring);
         int[][] Cb = reverseZigZagScan(height / 2, width / 2, Cbstring);
+//        System.out.println("Done");
         int[][] Cr = reverseZigZagScan(height / 2, width / 2, Crstring);
 
         int[][] RGB = convertYCbCrToRGB(Y, Cb, Cr);
@@ -116,7 +150,7 @@ public class JpegCodec implements Codec {
                         g = Math.min(255, Math.max(0, g));
                         b = Math.min(255, Math.max(0, b));
 
-                        RGB[2 * i + ii][2 * j + jj] = (r << 16) | (g << 8) | b;
+                        RGB[2 * i + ii][2 * j + jj] = (0xFF << 24) | (r << 16) | (g << 8) | b;
                     }
                 }
             }
@@ -161,6 +195,7 @@ public class JpegCodec implements Codec {
     }
 
     private int[][] reverseZigZagScan(int height, int width, String zigZagString) {
+//        System.out.println(zigZagString);
         String[] matrixCells = zigZagString.split(" ");
         int[][] reqMatrix = new int[height][width];
 
