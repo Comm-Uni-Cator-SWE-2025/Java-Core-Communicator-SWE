b10ae209a7ac141ce6bd8adda34ec3577466089a
udithdoddapaneni <udithdoddapaneni@gmail.com>
2025-10-10 05:40:14 +0530

    chunkManager added with Tests

---
+++ Networking/src/main/java/com/swe/networking/ChunkManager.java
@@ -0,0 +1,34 @@
+package com.swe.networking;
+
+import java.net.InetAddress;
+import java.util.Vector;
+
+public class ChunkManager {
+    int payloadSize; // size of payload in bytes
+    ChunkManager(int payloadSize){
+        this.payloadSize = payloadSize;
+    }
+    public Vector<byte[]> Chunk(int priority, int module,
+                                int connectionType, int broadcast,
+                                InetAddress ipAddr, int portNum,
+                                int messageId, byte[] data) {
+        Vector<byte[]> chunks = new Vector<>();
+        PacketParser parser = PacketParser.getPacketParser();
+        // It would be better to make HEADER_SIZE variable in PacketParser public or make function to get its value
+        // Currently I am hardcoding it
+
+        // Type is currently set to 0 : Send Packet to Cluster
+        // But we need a function in topology to identify the type
+        for (int i = 0; i < data.length; i+=payloadSize){
+            byte[] payloadChunk = new byte[payloadSize];
+            System.arraycopy(data, i, payloadChunk, 0, payloadSize);
+            chunks.add(payloadChunk);
+            chunks.add(parser.createPkt(
+                    0, priority, module,
+                    connectionType, broadcast, ipAddr,
+                    portNum, messageId, i/payloadSize, payloadSize + 20, payloadChunk
+            ));
+        }
+        return  chunks;
+    }
+}
