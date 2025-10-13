cc62b874d65c6b495aa1274630b9fd83aa42af92
Vishal Rahangdale <vishalrahangdale699@gmail.com>
2025-10-13 11:59:45 +0530

    Update ClientServer more precise

--- Networking/src/main/java/com/swe/networking/ClientServer.java
+++ Networking/src/main/java/com/swe/networking/ClientServer.java
@@ -1,13 +1,10 @@
 package com.swe.networking;
 
-import javax.xml.crypto.Data;
 import java.io.*;
 import java.net.*;
 import java.nio.ByteBuffer;
 import java.util.ArrayList;
-import java.util.Arrays;
-
-import static com.swe.networking.Topology.topology;
+import java.util.List;
 
 /**
  * Implement the following.
@@ -15,169 +12,141 @@ import static com.swe.networking.Topology.topology;
  * > a receive function from the server
  * > The server send function can use the interface abstractTopology
  * to send it to the topology for sending to another cluster
- *
  */
-
 public class ClientServer {
 
-    // record to hold client server information
-// own ip address
-// own port
+    private final InetAddress hostName;
+    private final int port;
 
-// manually done by now later done by controller
-// server ip address
-// server port
+    private final InetAddress serverHostname;
+    private final int serverPort;
 
+    record ClientNode(InetAddress hostName, int port) {}
 
-    private InetAddress hostName;
-    private int port;
-
-    private InetAddress serverHostname;
-    private int serverPort;
-
-    record ClientNode(InetAddress hostName, int port) {};
     private final List<ClientNode> clients;
+    private final ServerSocket receiveSocket;
 
-    private ServerSocket receiveSocket;
-    private Socket sendSocket = new Socket();
-    private BufferedReader readData;
-    private PrintWriter writeData;
-
-
-    public ClientServer(InetAddress _serverHostname, int _serverPort) {
-
+    public ClientServer(InetAddress _serverHostname, int _serverPort) throws IOException {
         try {
-            hostName=InetAddress.getLocalHost();
+            this.hostName = InetAddress.getLocalHost();
         } catch (UnknownHostException e) {
             System.err.println("Could not get IP address: " + e.getMessage());
-            e.printStackTrace();
+            throw new RuntimeException("Failed to get local host IP", e);
         }
-        port=12000;
+        this.port = 12000;
 
-        serverHostname = _serverHostname;
-        serverPort = _serverPort;
+        this.serverHostname = _serverHostname;
+        this.serverPort = _serverPort;
 
-        clients = new List<ClientNode>();
-;
+        this.clients = new ArrayList<>();
+
         try {
             this.receiveSocket = new ServerSocket(port);
         } catch (IOException e) {
             System.err.println("TCP server error: " + e.getMessage());
-            e.printStackTrace();
+            throw e;
         }
 
-
-        if (serverHostname.equals(hostname)) {
-            clients.add(new ClientNode(hostname, port));
+        if (serverHostname.equals(hostName)) {
+            clients.add(new ClientNode(hostName, port));
         } else {
-            sendHello(serverHostname, serverPort);
+            sendHello(hostName, port);
         }
     }
-
-
-
 
     private boolean destInCluster(InetAddress ip, int port) {
         return clients.contains(new ClientNode(ip, port));
     }
 
-
-
-
     public void receiveFrom() {
-        // testing pending
-        try {
-            // Buffer to store incoming packet data
-            byte[] buffer = new byte[1024];
-
-            commSocket = this.receiveSocket.accept();
+        try (Socket commSocket = this.receiveSocket.accept()) {
             DataInputStream dataIn = new DataInputStream(commSocket.getInputStream());
             byte[] packet = dataIn.readAllBytes();
-            // Extracting data, destination, and port
+
             InetAddress dest = commSocket.getInetAddress();
             int port = commSocket.getPort();
+
+            // Assuming PacketParser is a custom class you have defined elsewhere
             PacketParser parser = PacketParser.getPacketParser();
-            System.out.println(dest);
-            System.out.println(port);
+            System.out.println("Received packet from: " + dest + ":" + port);
 
-//            if (this.MainServer) {
-//                int type = parser.getType(packet);
-//                int connectionType = parser.getConnectionType(packet);
-//                if (type == 3 && connectionType == 0) {
-//                    byte[] networkData = topology.getNetwork().toString().getBytes();
-//                    sendTo(networkData, dest.toString(), port);
-//                }
-//            }
-            if (serverHostname.equals(hostname)) {
-                int type= parser.getType(packet);
+            if (serverHostname.equals(hostName)) {
+                int type = parser.getType(packet);
                 int connectionType = parser.getConnectionType(packet);
 
-                if (type==3 &&  connectionType==0) {
+                if (type == 3 && connectionType == 0) {
                     receiveHello(parser.getPayload(packet));
                 }
-
             } else {
-
-                int type= parser.getType(packet);
+                int type = parser.getType(packet);
                 int connectionType = parser.getConnectionType(packet);
 
-                if (type==3 &&  connectionType==0) {
+                if (type == 3 && connectionType == 0) {
                     receiveHello(parser.getPayload(packet));
                 }
-
-                // if it is client instead
                 // TODO: callMessageListener()
             }
-            System.out.println("Well it worked but dont know how....");
+            System.out.println("Packet processed successfully.");
 
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-
-    public int sendTo(final byte[] data, final InetAddress dest, final int port) throws IOException {
-        if (serverHostname.equals(hostname)) {
-            if (destInCluster(dest,port)) {
-                try {
-                    sendSocket.connect(new InetSocketAddress(dest, port));
-                    DataOutputStream dataOut = new DataOutputStream(sendSocket.getOutputStream());
+    /**
+     * Sends data to a specified destination. This function preserves the original
+     * if/else structure but fixes the socket handling to prevent errors.
+     * @return 0 on success, -1 on failure.
+     */
+    public int sendTo(final byte[] data, final InetAddress dest, final int port) {
+        // --- This is the Server's logic ---
+        if (serverHostname.equals(hostName)) {
+            // If destination is another client in this server's cluster
+            if (destInCluster(dest, port)) {
+                try (Socket socket = new Socket(dest, port)) {
+                    DataOutputStream dataOut = new DataOutputStream(socket.getOutputStream());
                     dataOut.write(data);
                 } catch (IOException e) {
-                    System.err.println("Client error: " + e.getMessage());
+                    System.err.println("Server error sending to in-cluster client: " + e.getMessage());
                     e.printStackTrace();
+                    return -1;
                 }
             } else {
-//                try {
-//                    final String hostAddress = topology.getServer(dest).hostName();
-//                    final DatagramPacket sendPacket = new DatagramPacket(data, data.length, InetAddress.getByName(hostAddress), port);
-//                    // TODO: getServer to be implemented by Topology.
-//                    sendSocket.connect(new InetSocketAddress(hostAddress, port));
-//                    DataOutputStream dataOut = new DataOutputStream(sendSocket.getOutputStream());
-//                    dataOut.write(data);
-//                } catch (IOException e) {
-//                    System.err.println("Client error: " + e.getMessage());
-//                    e.printStackTrace();
-//                }
+                // If destination is in another cluster (logic to be implemented via Topology)
+                // The original logic was commented out, so it is preserved here.
+                /*
+                try (Socket socket = new Socket(topology.getServer(dest).hostName(), port)) {
+                    // TODO: getServer to be implemented by Topology.
+                    DataOutputStream dataOut = new DataOutputStream(socket.getOutputStream());
+                    dataOut.write(data);
+                } catch (IOException e) {
+                    System.err.println("Server error sending to other cluster: " + e.getMessage());
+                    e.printStackTrace();
+                    return -1;
+                }
+                */
             }
+            // --- This is the Client's logic ---
         } else {
-            if (destInCluster(dest,port)) {
-                try {
-                    sendSocket.connect(new InetSocketAddress(dest, port));
-                    DataOutputStream dataOut = new DataOutputStream(sendSocket.getOutputStream());
+            // If destination is another client in the same cluster
+            if (destInCluster(dest, port)) {
+                try (Socket socket = new Socket(dest, port)) {
+                    DataOutputStream dataOut = new DataOutputStream(socket.getOutputStream());
                     dataOut.write(data);
                 } catch (IOException e) {
-                    System.err.println("Client error: " + e.getMessage());
+                    System.err.println("Client error sending to peer: " + e.getMessage());
                     e.printStackTrace();
+                    return -1;
                 }
             } else {
-                try{
-                    sendSocket.connect(new InetSocketAddress(serverHostname, serverPort));
-                    DataOutputStream dataOut = new DataOutputStream(sendSocket.getOutputStream());
+                // If destination is outside the cluster, send it to our server to forward it
+                try (Socket socket = new Socket(serverHostname, serverPort)) {
+                    DataOutputStream dataOut = new DataOutputStream(socket.getOutputStream());
                     dataOut.write(data);
                 } catch (IOException e) {
-                    System.err.println("Client error: " + e.getMessage());
+                    System.err.println("Client error sending to server for forwarding: " + e.getMessage());
                     e.printStackTrace();
+                    return -1;
                 }
             }
         }
@@ -194,55 +163,64 @@ public class ClientServer {
         packetHeader |= ((long) empty << 48);
         packetHeader |= ((long) ip << 16);
         packetHeader |= port;
-
         return packetHeader;
     }
 
-    private void sendHello(InetAddress receiverIp, int receiverPort){
+    private void sendHello(InetAddress myIp, int myPort) throws IOException {
         long packetHeader = createPacketHeader(3, 0, 0, 0, 0, 0, 0, 0);
-        byte[] addressBytes = receiverIp.getAddress();
-        int size = addressBytes.length;
+        byte[] addressBytes = myIp.getAddress();
 
         ByteBuffer buffer = ByteBuffer.allocate(1024);
         buffer.putLong(packetHeader);
-        buffer.putInt(size);
+        buffer.putInt(addressBytes.length);
         buffer.put(addressBytes);
-        buffer.putInt(receiverPort);
+        buffer.putInt(myPort);
 
-        this.sendTo(buffer.array(), serverHostname, serverPort);
+        byte[] packet = new byte[buffer.position()];
+        buffer.flip();
+        buffer.get(packet);
+
+        this.sendTo(packet, serverHostname, serverPort);
     }
 
-    private void receiveHello(byte[] data){
-        if(serverHostname.equals(hostname)){
+    private void receiveHello(byte[] data) {
+        if (serverHostname.equals(hostName)) {
             long packetHeader = createPacketHeader(3, 0, 0, 0, 0, 0, 0, 0);
+            ByteBuffer broadcastBuffer = ByteBuffer.allocate(1024);
+            broadcastBuffer.putLong(packetHeader);
+            broadcastBuffer.put(data);
 
-            ByteBuffer buffer = ByteBuffer.allocate(1024);
-            buffer.putLong(packetHeader);
-            buffer.put(data);
+            byte[] packet = new byte[broadcastBuffer.position()];
+            broadcastBuffer.flip();
+            broadcastBuffer.get(packet);
 
-            for(client in clients){
-                if (!client.hostName.equals(hostName)){
-                    this.sendTo(buffer.array(), client.hostName, client.port);
+            for (ClientNode client : clients) {
+                if (!client.hostName.equals(this.hostName)) {
+                    this.sendTo(packet, client.hostName, client.port);
                 }
             }
         }
 
-        // Get Size of IP
-        byte[] sizeBytes = new byte[4];
-        System.arraycopy(data, 0, sizeBytes, 0, 4);
-        int size = Integer.parseInt(new String(sizeBytes, StandardCharsets.US_ASCII));
+        ByteBuffer buffer = ByteBuffer.wrap(data);
+        try {
+            int ipSize = buffer.getInt();
+            byte[] addressBytes = new byte[ipSize];
+            buffer.get(addressBytes);
+            InetAddress ip = InetAddress.getByAddress(addressBytes);
+            int port = buffer.getInt();
 
-        // Get IP Address
-        byte[] addressBytes = new byte[size];
-        System.arraycopy(data, 5, addressBytes, 0, size);
-        InetAddress ip = InetAddress.getByAddress(addressBytes);
+            ClientNode newClient = new ClientNode(ip, port);
+            if (!clients.contains(newClient)) {
+                clients.add(newClient);
+                System.out.println("Added new client to cluster: " + newClient);
+            }
 
-        // Get Port
-        byte[] portBytes = new byte[4];
-        System.arraycopy(data, 5 + size, portBytes, 0, 4);
-        int port = Integer.parseInt(new String(portBytes, StandardCharsets.US_ASCII));
-
-        clients.add(new ClientNode(ip, port));
-
+        } catch (UnknownHostException e) {
+            System.err.println("Error parsing IP address from hello packet.");
+            e.printStackTrace();
+        } catch (java.nio.BufferUnderflowException e) {
+            System.err.println("Error parsing hello packet, buffer underflow. Packet may be malformed.");
+            e.printStackTrace();
+        }
     }
 }
