798acfecda54c60bd923fb3589ac956be5d018cd
909ahmed <crypticahmed@gmail.com>
2025-10-25 02:38:47 +0530

    Added code for the p2pserver and p2pcluster and added timer.java

--- java/module-networking/src/main/java/com/swe/networking/P2PCluster.java
+++ java/module-networking/src/main/java/com/swe/networking/P2PCluster.java
@@ -1,7 +1,9 @@
 package com.swe.networking;
 
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.Socket;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 
 /**
@@ -13,11 +15,6 @@ public class P2PCluster {
      * List of all clients belonging to the same cluster.
      */
     private List<ClientNode> clients;
-
-    /**
-     * Timeout to keep track of all clients.
-     */
-    private HashMap<String, Long> clientTimeouts;
 
     /**
      * The status of current device as client / server.
@@ -34,10 +31,14 @@ public class P2PCluster {
      */
     private boolean isServer = false;
 
+    /**
+     * Packet parser singleton object.
+     */
+    private static PacketParser packetParser = PacketParser.getPacketParser();
+
     public P2PCluster() {
         System.out.println("Creating a new P2P Cluster...");
         clients = new ArrayList<>();
-        clientTimeouts = new HashMap<>();
     }
 
     /**
@@ -48,7 +49,51 @@ public class P2PCluster {
      */
     public void addUser(final ClientNode client, final ClientNode server) {
         System.out.println("Adding new user to the network...");
+        // send hello to server
+        final byte[] helloPacket = packetParser.createPkt(
+            NetworkType.USE.ordinal(), 0, 0, NetworkConnectionType.HELLO.ordinal(), 
+            0, null, 0, 0, 0, 0, null); 
 
+        try {
+            final Socket socket = new Socket(client.hostName(), client.port());
+            final OutputStream out = socket.getOutputStream();
+            out.write(helloPacket);
+            out.flush();
+            socket.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        try {
+            final Socket socket = new Socket(client.hostName(), client.port());
+            final InputStream in = socket.getInputStream();
+            final byte[] packet = new byte[4096];
+            final int bytesRead = in.read(packet);
+            if (bytesRead > 0) {
+                System.out.println("Received structure from server: " + server.hostName());
+                clients.add(client);
+                // deserialize the object from packet
+                final NetworkStructure networkStructure = packetParser.getPayload(packet);
+                for (int i = 0; i < networkStructure.servers().size(); i++) {
+                    if (networkStructure.servers().get(i).equals(client)) {
+                        clusterServer = client;
+                        user = new P2PServer(client, server);
+                        for (ClientNode c : networkStructure.clusters().get(i)) {
+                            ((P2PServer) user).monitor(c);
+                        }
+                        break;
+                    }
+                    if (networkStructure.clusters().get(i).contains(client)) {
+                        clusterServer = networkStructure.servers().get(i);
+                        this.isServer = false;
+                        user = new P2PClient(client, server);
+                        break;
+                    }
+                }
+            }
+            socket.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     /**
@@ -60,10 +105,5 @@ public class P2PCluster {
         if (clients.size() < size - 1) {
             System.out.println("Cannot resize cluster...");
         }
-    }
-
-    public void updateClientTimeout(final String client) {
-
-    }
-
+    }    
 }
