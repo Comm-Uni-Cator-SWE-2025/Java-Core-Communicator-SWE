92cea8ad77536ada3b57874ba24e7291cf98e13f
Logan <112201016@smail.iitpkd.ac.in>
2025-10-27 13:03:42 +0530

    Checkstyle for simplenetworking/Packetparser

--- java/module-networking/src/main/java/com/swe/networking/SimpleNetworking/PacketParser.java
+++ java/module-networking/src/main/java/com/swe/networking/SimpleNetworking/PacketParser.java
@@ -29,41 +29,86 @@ The structure of the packet is given below
 +--+--+--+--+-
 */
 
-
+/**
+ * Class for packet parser.
+ */
 public class PacketParser {
-    private static PacketParser parser = null; 
+    /** Static class object for packet parser. */
+    private static PacketParser parser = null;
 
-    private PacketParser(){
+    /** Private constructor class. */
+    private PacketParser() {
 
     }
 
-    public static PacketParser getPacketParser(){
-        if(parser == null) {
+    /**
+     * Function to get the packetparser.
+     *
+     * @return the singleton object.
+     */
+    public static PacketParser getPacketParser() {
+        if (parser == null) {
             parser = new PacketParser();
         }
         return parser;
     }
 
+    /**
+     * Function to get the type of packet.
+     *
+     * @param pkt the packet
+     * @return the type
+     */
     public int getType(final byte[] pkt) {
         return (pkt[0] >> 6) & 0b11;
     }
 
+    /**
+     * Function to get the priority of packet.
+     *
+     * @param pkt the packet
+     * @return the priority
+     */
     public int getPriority(final byte[] pkt) {
         return (pkt[0] >> 3) & 0b111;
     }
 
+    /**
+     * Function to get the module of packet.
+     *
+     * @param pkt the packet
+     * @return the module
+     */
     public int getModule(final byte[] pkt) {
         return ((pkt[0] & 0b111) << 1) | ((pkt[1] >> 7) & 0b1);
     }
 
+    /**
+     * Function to get the connection type of packet.
+     *
+     * @param pkt the packet
+     * @return the connection type
+     */
     public int getConnectionType(final byte[] pkt) {
         return (pkt[1] >> 4) & 0b111;
     }
 
+    /**
+     * Function to get the broadcast of packet.
+     *
+     * @param pkt the packet
+     * @return the broadcast
+     */
     public int getBroadcast(final byte[] pkt) {
         return (pkt[1] >> 3) & 0b1;
     }
 
+    /**
+     * Function to get the ip address of packet.
+     *
+     * @param pkt the packet
+     * @return the ip address
+     */
     public InetAddress getIpAddress(final byte[] pkt) throws UnknownHostException {
         final byte[] ipBytes = new byte[4];
         for (int i = 0; i < 4; i++) {
@@ -72,16 +117,41 @@ public class PacketParser {
         return InetAddress.getByAddress(ipBytes);
     }
 
+    /**
+     * Function to get the port of packet.
+     *
+     * @param pkt the packet
+     * @return the port
+     */
     public int getPortNum(final byte[] pkt) {
         return ((pkt[6] & 0xFF) << 8) | (pkt[7] & 0xFF);
     }
 
+    /**
+     * Function to get the payload of packet.
+     *
+     * @param pkt the packet
+     * @return the payload
+     */
     public byte[] getPayload(final byte[] pkt) {
         final byte[] payload = new byte[pkt.length - 8];
         System.arraycopy(pkt, 8, payload, 0, payload.length);
         return payload;
     }
 
+    /**
+     * Function to create the packet.
+     *
+     * @param type           the type of packet
+     * @param priority       the priority of packet
+     * @param module         the module of packet
+     * @param connectionType the connection type of packet
+     * @param broadCast      the broadcast of packet
+     * @param ipAddr         the ip address of packet
+     * @param portNum        the port of packet
+     * @param data           the payload of packet
+     * @return the packet
+     */
     public byte[] createPkt(final int type, final int priority, final int module,
             final int connectionType, final int broadCast, final InetAddress ipAddr,
             final int portNum, final byte[] data) {
@@ -101,10 +171,10 @@ public class PacketParser {
         System.arraycopy(ipBytes, 0, pkt, 2, 4);
 
         // Byte 6,7
-        pkt[6] = ( byte )(portNum >> 8);
-        pkt[7] = ( byte )(portNum & 0xFF);
+        pkt[6] = (byte) (portNum >> 8);
+        pkt[7] = (byte) (portNum & 0xFF);
 
-        System.arraycopy( data, 0, pkt, 8, data.length );
+        System.arraycopy(data, 0, pkt, 8, data.length);
         return pkt;
     }
 }
