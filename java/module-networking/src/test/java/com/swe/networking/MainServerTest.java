8f7b4e297b3815ac9405dd20cd1cfd871eeca432
Logan <112201016@smail.iitpkd.ac.in>
2025-10-29 11:47:41 +0530

    timer implemented for main serverand tested AB#99

--- java/module-networking/src/test/java/com/swe/networking/MainServerTest.java
+++ java/module-networking/src/test/java/com/swe/networking/MainServerTest.java
@@ -7,6 +7,7 @@ import java.net.InetAddress;
 import java.net.InetSocketAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.net.UnknownHostException;
 
 /**
  * Test class for MainServer.
@@ -20,10 +21,16 @@ public class MainServerTest {
         try {
             final int serverport1 = 8001;
             final int serverport2 = 8002;
+            final int serverport3 = 8003;
+            final int serverport4 = 8004;
             final Thread recieveThread1 = new Thread(() -> receive(serverport1));
             final Thread recieveThread2 = new Thread(() -> receive(serverport2));
+            final Thread recieveThread3 = new Thread(() -> receive(serverport3));
+            final Thread recieveThread4 = new Thread(() -> receive(serverport4));
             recieveThread1.start();
             recieveThread2.start();
+            recieveThread3.start();
+            recieveThread4.start();
             final Integer sleepTime = 500;
             Thread.sleep(sleepTime);
             final ClientNode server = new ClientNode("127.0.0.1", 8000);
@@ -35,8 +42,17 @@ public class MainServerTest {
             mainServer.send(data.getBytes(), dest2);
             mainServer.closeClient(dest1);
             mainServer.closeClient(dest2);
+            Thread.sleep(sleepTime);
+            final ClientNode dest3 = new ClientNode("127.0.0.1", 8003);
+            final ClientNode dest4 = new ClientNode("127.0.0.1", 8004);
+            final ClientNode[] clients = { dest3, dest4 };
+            mainServer.send(data.getBytes(), clients);
+            mainServer.closeClient(dest3);
+            mainServer.closeClient(dest4);
             recieveThread1.join();
             recieveThread2.join();
+            recieveThread3.join();
+            recieveThread4.join();
             mainServer.close();
         } catch (InterruptedException ex) {
         }
@@ -85,42 +101,86 @@ public class MainServerTest {
             final Socket destSocket = new Socket();
             final Integer port = 8000;
             final Integer timeout = 5000;
-            final int randomFactor = (int) Math.pow(10, 6);
             destSocket.connect(new InetSocketAddress("127.0.0.1", port), timeout);
             final DataOutputStream dataOut = new DataOutputStream(destSocket.getOutputStream());
-            final String data = "Hello World !!!";
-            final PacketParser parser = PacketParser.getPacketParser();
+            ClientNode client = new ClientNode("127.0.0.1", destSocket.getLocalPort());
+            final byte[] packet = getHelloPacket(client);
+            dataOut.write(packet);
+            destSocket.close();
+        } catch (IOException ex) {
+        }
+    }
+
+    public byte[] getRemovePacket(ClientNode client) {
+        try {
+            Topology topology = Topology.getTopology();
+            final int randomFactor = (int) Math.pow(10, 6);
+            PacketParser parser = PacketParser.getPacketParser();
+            final PacketInfo pkt = new PacketInfo();
+            pkt.setType(NetworkType.USE.ordinal());
+            pkt.setPriority(0);
+            pkt.setModule(0);
+            pkt.setConnectionType(NetworkConnectionType.REMOVE.ordinal());
+            pkt.setBroadcast(0);
+            pkt.setIpAddress(InetAddress.getByName(client.hostName()));
+            pkt.setPortNum(client.port());
+            pkt.setMessageId((int) (Math.random() * randomFactor));
+            pkt.setChunkNum(0);
+            pkt.setChunkLength(1);
+            int idx = topology.getClusterIndex(client);
+            ClientNetworkRecord record = new ClientNetworkRecord(client, idx);
+            pkt.setPayload(NetworkSerializer.getNetworkSerializer().serializeClientNetworkRecord(record));
+            final byte[] packet = parser.createPkt(pkt);
+            return packet;
+        } catch (UnknownHostException ex) {
+            return null;
+        }
+    }
+
+    public byte[] getHelloPacket(ClientNode client) {
+        try {
+            final int randomFactor = (int) Math.pow(10, 6);
+            PacketParser parser = PacketParser.getPacketParser();
             final PacketInfo pkt = new PacketInfo();
             pkt.setType(NetworkType.USE.ordinal());
             pkt.setPriority(0);
             pkt.setModule(0);
             pkt.setConnectionType(NetworkConnectionType.HELLO.ordinal());
             pkt.setBroadcast(0);
-            pkt.setIpAddress(InetAddress.getByName("127.0.0.1"));
-            pkt.setPortNum(port);
+            pkt.setIpAddress(InetAddress.getByName(client.hostName()));
+            pkt.setPortNum(client.port());
             pkt.setMessageId((int) (Math.random() * randomFactor));
             pkt.setChunkNum(0);
             pkt.setChunkLength(1);
+            final String data = "Hello Sekai !!!";
             pkt.setPayload(data.getBytes());
             final byte[] packet = parser.createPkt(pkt);
-            dataOut.write(packet);
-            final PacketInfo pkt1 = new PacketInfo();
-            pkt.setType(NetworkType.SAMECLUSTER.ordinal());
-            pkt.setPriority(0);
-            pkt.setModule(0);
-            pkt.setConnectionType(NetworkConnectionType.HELLO.ordinal());
-            pkt.setBroadcast(0);
-            pkt.setIpAddress(InetAddress.getByName("127.0.0.1"));
-            pkt.setPortNum(port);
-            pkt.setMessageId((int) (Math.random() * randomFactor));
-            pkt.setChunkNum(0);
-            pkt.setChunkLength(1);
-            pkt.setPayload(data.getBytes());
-            // final byte[] packet1 = parser.createPkt(pkt1);
-            // dataOut.write(packet1);
-            destSocket.close();
-        } catch (IOException ex) {
+            return packet;
+        } catch (UnknownHostException ex) {
+            return null;
         }
     }
 
+    @org.junit.jupiter.api.Test
+    public void testMainServerTimeout() {
+        final ClientNode server = new ClientNode("127.0.0.1", 8000);
+        final MainServer mainServer = new MainServer(server, server);
+        timeoutsend();
+    }
+
+    public void timeoutsend() {
+        try {
+            final Socket destSocket = new Socket();
+            final Integer port = 8000;
+            final Integer timeout = 5000;
+            destSocket.connect(new InetSocketAddress("127.0.0.1", port), timeout);
+            final DataOutputStream dataOut = new DataOutputStream(destSocket.getOutputStream());
+            ClientNode client = new ClientNode("127.0.0.1", destSocket.getLocalPort());
+            final byte[] packet = getHelloPacket(client);
+            dataOut.write(packet);
+            Thread.sleep(6000);
+        } catch (IOException ex) {
+        } catch (InterruptedException ex) {
+        }
+    }
 }
