cd56bd4c76b786cfb2144362eda26eff46f6bf2c
Logan <112201016@smail.iitpkd.ac.in>
2025-10-29 18:04:23 +0530

    splitting packets on receiving

---
+++ java/module-networking/src/test/java/com/swe/networking/SplitPacketTest.java
@@ -0,0 +1,12 @@
+package com.swe.networking;
+
+/**
+ * Test class for splitting the packets.
+ */
+public class SplitPacketTest {
+
+    @org.junit.jupiter.api.Test
+    public void splitPacketTest() {
+        
+    }
+}
