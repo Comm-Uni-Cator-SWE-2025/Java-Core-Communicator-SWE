55d04d2001ed2021a567ecb7a204234c90603d29
alonot <priyanshup27092004@gmail.com>
2025-11-02 01:16:25 +0530

    encodeDecodeRLE bug fix and test

--- java/module-screen-video/src/main/java/com/swe/ScreenNVideo/MediaCaptureManager.java
+++ java/module-screen-video/src/main/java/com/swe/ScreenNVideo/MediaCaptureManager.java
@@ -117,7 +117,7 @@ public class MediaCaptureManager implements CaptureManager {
     }
 
     private void sendImageToViewers(final byte[] feed) {
-//        System.out.println("Size : " + feed.length / Utils.KB + " KB");
+        System.out.println("Size : " + feed.length / Utils.KB + " KB");
 //        networking.sendData(feed, viewers.toArray(new ClientNode[0]), ModuleType.CHAT, 2);
 //        SimpleNetworking.getSimpleNetwork().closeNetworking();
 //        System.out.println("Sent to viewers");
