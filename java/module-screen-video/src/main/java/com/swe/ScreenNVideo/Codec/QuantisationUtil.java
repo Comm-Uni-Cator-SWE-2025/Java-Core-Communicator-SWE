9980ae8cf9b1fbf1b1b6a4b40115f9aed0d1f10c
andhkr <anup2964@zohomail.in>
2025-11-02 01:07:14 +0530

    fix some checkstyle error

--- java/module-screen-video/src/main/java/com/swe/ScreenNVideo/Codec/QuantisationUtil.java
+++ java/module-screen-video/src/main/java/com/swe/ScreenNVideo/Codec/QuantisationUtil.java
@@ -1,14 +1,20 @@
 package com.swe.ScreenNVideo.Codec;
 
 /**
- * It includes Quantisation Table
- * It includes Method to scale it
- * required for compressing and decompressing
+ * Manages the quantization tables (Luminance and Chrominance) required for
+ * JPEG-style compression.
+ * This class implements the standard JPEG quality scaling logic and also
+ * provides a method to "absorb" DCT scaling factors into the tables.
+ * It is implemented as a singleton to ensure only one set of tables exists.
+ * @author Anup Kumar
  */
 public class QuantisationUtil {
-    // Annex K, Table K.1 of the JPEG Standard (ITU-T T.81 / ISO 10918-1, 1992)
-    // Microsoft Word - T081E.DOC
-    int[][] quantChrome = {
+    /**
+     * Annex K, Table K.1 of the JPEG Standard (ITU-T T.81 / ISO 10918-1, 1992)
+     * Default Chrominance Quantization Table.
+     * NOTE: This table is MUTABLE and will be changed by scaling methods.
+     */
+    private static final int[][] BASECHROME = {
             {17, 18, 24, 47, 99, 99, 99, 99},
             {18, 21, 26, 66, 99, 99, 99, 99},
             {24, 26, 56, 99, 99, 99, 99, 99},
@@ -16,99 +22,198 @@ public class QuantisationUtil {
             {99, 99, 99, 99, 99, 99, 99, 99},
             {99, 99, 99, 99, 99, 99, 99, 99},
             {99, 99, 99, 99, 99, 99, 99, 99},
-            {99, 99, 99, 99, 99, 99, 99, 99}
+            {99, 99, 99, 99, 99, 99, 99, 99},
     };
-    int[][] quantLumin = {
+
+    /**
+     * Annex K, Table K.1 of the JPEG Standard (ITU-T T.81 / ISO 10918-1, 1992)
+     * Default Luminance Quantization Table.
+     * NOTE: This table is MUTABLE and will be changed by scaling methods.
+     */
+    private static final int[][] BASELUMIN = {
             {16, 11, 10, 16, 24, 40, 51, 61},
             {12, 12, 14, 19, 26, 58, 60, 55},
             {14, 13, 16, 24, 40, 57, 69, 56},
             {14, 17, 22, 29, 51, 87, 80, 62},
-            {18, 22, 37, 56, 68,109,103, 77},
-            {24, 35, 55, 64, 81,104,113, 92},
-            {49, 64, 78, 87,103,121,120,101},
-            {72, 92, 95, 98,112,100,103, 99}
+            {18, 22, 37, 56, 68, 109, 103, 77},
+            {24, 35, 55, 64, 81, 104, 113, 92},
+            {49, 64, 78, 87, 103, 121, 120, 101},
     };
 
-    private static final QuantisationUtil _quantisationUtil = new QuantisationUtil();
+    /**
+     * used for scaling.
+     */
+    private int[][] scaledQuantChrome = new int[8][8];
+    /**
+     * used for scaling.
+     */
+    private int[][] scaledQuantLumin = new int[8][8];
+    /**
+     * Singleton instance.
+     */
+    private static final QuantisationUtil quantisationUtil = new QuantisationUtil();
 
-    private QuantisationUtil(){}
-
-    public static QuantisationUtil getInstance(){
-        return _quantisationUtil;
+    /**
+     * Private constructor to enforce singleton pattern.
+     */
+    private QuantisationUtil() {
+        // Initialize the scaled tables when created
+        resetQuantTables();
     }
 
-    public void setCompressonResulation(int q){
-        int scaleFactor = 1;
-        if(q>=1 && q<50){
-            scaleFactor = (5000)/q;
-        }else if(q>=50 && q<100){
-            scaleFactor = 200-2*q;
+    /**
+     * This will be set the quant tables to default.
+     */
+    private void resetQuantTables() {
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 8; j++) {
+                scaledQuantChrome[i][j] = BASECHROME[i][j];
+                scaledQuantLumin[i][j] = BASELUMIN[i][j];
+            }
+        }
+    }
+
+    /**
+     * Gets the singleton instance of the QuantisationUtil.
+     * @return The single instance of this class.
+     */
+    public static QuantisationUtil getInstance() {
+        return quantisationUtil;
+    }
+
+    /**
+     * Scales the quantization tables based on a JPEG quality factor 'q'.
+     * This method MODIFIES the class's quantChrome and quantLumin tables in-place.
+     *
+     * @param q : A quality factor from 1 to 99.
+     */
+    public void setCompressonResulation(int q) {
+
+        resetQuantTables();
+
+        int scaleFactor = 200 - 2 * q;
+
+        // Standard JPEG quality scale factor calculation.
+        if (q >= 1 && q < 50) {
+            scaleFactor = 5000 / q;
         }
 
-        for(int i = 0;i<8;++i){
-            for(int j = 0;j<8;++j){
-                int scaledCVal = (quantChrome[i][j]*scaleFactor + 50)/100;
-                int scaledLVal = (quantLumin[i][j]*scaleFactor + 50)/100;
+        for (int i = 0; i < 8; ++i) {
+            for (int j = 0; j < 8; ++j) {
+                // Apply scale factor with rounding (+50 / 100)
+                int scaledCVal = (scaledQuantChrome[i][j] * scaleFactor + 50) / 100;
+                int scaledLVal = (scaledQuantLumin[i][j] * scaleFactor + 50) / 100;
 
+                // Clamp values to the valid JPEG range [1, 255]
+                if (scaledCVal<1) {
+                    scaledCVal = 1;
+                } else if (scaledCVal>255) {
+                    scaledCVal = 255;
+                }
+
+                if (scaledLVal < 1) {
+                    scaledLVal = 1;
+                } else if (scaledLVal > 255) {
+                    scaledLVal = 255;
+                }
+
+                scaledQuantChrome[i][j] = scaledCVal;
+                scaledQuantLumin[i][j] = scaledLVal;
+            }
+        }
+    }
+
+    /**
+     * Scales the quantization tables by a given set of 1D scaling factors.
+     * This is used to "absorb" the scaling factors from a fast DCT (like AAN)
+     * into the quantization table, as you mentioned.
+     *
+     * This method MODIFIES the class's quantChrome and quantLumin tables in-place.
+     *
+     * @param scalingFactors An 8-element array of 1D scaling factors from the DCT.
+     */
+    public void scaleQuantTable(final double[] scalingFactors) {
+        for (int i = 0; i < 8; ++i) {
+            for (int j = 0; j<8; ++j) {
+
+                // Apply 2D scaling by dividing by the outer product (scale[i] * scale[j])
+                // Add 0.5 before casting to int to perform rounding.
+                int scaledCVal = (int) ((scaledQuantChrome[i][j] / (scalingFactors[i] * scalingFactors[j])) + 0.5);
+                int scaledLVal = (int) ((scaledQuantLumin[i][j] / (scalingFactors[i] * scalingFactors[j])) + 0.5);
+
+                // Clamp values to the valid JPEG range [1, 255]
                 if(scaledCVal<1) scaledCVal = 1;
                 else if(scaledCVal>255) scaledCVal = 255;
 
                 if(scaledLVal<1) scaledLVal = 1;
                 else if(scaledLVal>255) scaledLVal = 255;
 
-                quantChrome[i][j] = scaledCVal;
-                quantLumin[i][j] = scaledLVal;
+                scaledQuantChrome[i][j] = scaledCVal;
+                scaledQuantLumin[i][j] = scaledLVal;
             }
         }
     }
 
-    public void scaleQuantTable(double[] scalingFactors){
-        for(int i = 0;i<8;++i){
-            for(int j = 0;j<8;++j){
-
-                int scaledCVal = (int)((quantChrome[i][j]/(scalingFactors[i]*scalingFactors[j]))+0.5);
-                int scaledLVal = (int)((quantLumin[i][j]/(scalingFactors[i]*scalingFactors[j]))+0.5);
-
-                if(scaledCVal<1) scaledCVal = 1;
-                else if(scaledCVal>255) scaledCVal = 255;
-
-                if(scaledLVal<1) scaledLVal = 1;
-                else if(scaledLVal>255) scaledLVal = 255;
-
-                quantChrome[i][j] = scaledCVal;
-                quantLumin[i][j] = scaledLVal;
+    /**
+     * Applies quantization to a Chrominance block of DCT coefficients.
+     *
+     * @param dMatrix The 8x8 block of DCT coefficients.
+     * @param row     The starting row of the block.
+     * @param col     The starting column of the block.
+     */
+    public void quantisationChrome (final short[][] dMatrix, short row, short col) {
+        for (int i = 0; i < 8; ++i) {
+            for (int j = 0; j < 8; ++j) {
+                // Quantize: divide by the table value and round.
+                dMatrix[i + row][j + col] = (short) Math.round((float) dMatrix[i + row][j + col] / scaledQuantChrome[i][j]);
             }
         }
     }
 
-    public void QuantisationChrome(short[][] DMatrix,short row, short col){
-        for(int i = 0;i<8;++i){
-            for(int j = 0;j<8;++j){
-                DMatrix[i+row][j+col] = (short)Math.round((float) DMatrix[i+row][j+col] /quantChrome[i][j]);
+    /**
+     * Applies quantization to a Luminance block of DCT coefficients.
+     *
+     * @param dMatrix The 8x8 block of DCT coefficients.
+     * @param row     The starting row of the block.
+     * @param col     The starting column of the block.
+     */
+    public void quantisationLumin (final short[][] dMatrix, short row, short col) {
+        for (int i = 0; i < 8; ++i) {
+            for (int j = 0; j < 8; ++j) {
+                // Quantize: divide by the table value and round.
+                dMatrix[i + row][j + col] = (short) Math.round((float) dMatrix[i + row][j + col] / scaledQuantLumin[i][j]);
             }
         }
     }
 
-    public void QuantisationLumin(short[][] DMatrix,short row, short col){
-        for(int i = 0;i<8;++i){
-            for(int j = 0;j<8;++j){
-                DMatrix[i+row][j+col] = (short)Math.round((float) DMatrix[i+row][j+col] /quantLumin[i][j]);
+    /**
+     * Applies de-quantization to a Chrominance block of DCT coefficients.
+     *
+     * @param dMatrix The 8x8 block of quantized coefficients.
+     * @param row     The starting row of the block.
+     * @param col     The starting column of the block.
+     */
+    public void deQuantisationChrome (short[][] dMatrix,short row, short col) {
+        for (int i = 0; i < 8; ++i) {
+            for (int j = 0; j < 8; ++j) {
+                // De-quantize: multiply by the table value.
+                dMatrix[i + row][j + col] = (short) (dMatrix[i + row][j + col] * scaledQuantChrome[i][j]);
             }
         }
     }
 
-    public void DeQuantisationChrome(short[][] DMatrix,short row, short col){
-        for(int i = 0;i<8;++i){
-            for(int j = 0;j<8;++j){
-                DMatrix[i+row][j+col] = (short) (DMatrix[i+row][j+col] * quantChrome[i][j]);
-            }
-        }
-    }
-
-    public void DeQuantisationLumin(short[][] DMatrix,short row, short col){
-        for(int i = 0;i<8;++i){
-            for(int j = 0;j<8;++j){
-                DMatrix[i+row][j+col] = (short) (DMatrix[i+row][j+col]  * quantLumin[i][j]);
+    /**
+     * Applies de-quantization to a Luminance block of DCT coefficients.
+     *
+     * @param dMatrix The 8x8 block of quantized coefficients.
+     * @param row     The starting row of the block.
+     * @param col     The starting column of the block.
+     */
+    public void deQuantisationLumin (final short[][] dMatrix,short row, short col) {
+        for (int i = 0; i < 8; ++i) {
+            for (int j = 0; j < 8; ++j) {
+                // De-quantize: multiply by the table value.
+                dMatrix[i + row][j + col] = (short) (dMatrix[i + row][j + col]  * scaledQuantLumin[i][j]);
             }
         }
     }
