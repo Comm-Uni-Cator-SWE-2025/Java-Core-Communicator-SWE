127ed20aa4dbf1c60f27132e839fe0a9a4aea13a
momkingtai <momkingtai@gmail.com>
2025-11-08 22:27:49 +0530

    added new telemetry files

--- java/module-core/src/main/java/com/swe/core/Analytics_old/MeetAnalytics.java
+++ java/module-core/src/main/java/com/swe/core/Analytics_old/MeetAnalytics.java
@@ -1,49 +1,49 @@
-package com.swe.core.Analytics;
+package com.swe.core.Analytics_old;
 
 import java.util.HashMap;
 import java.util.Map;
 
 public class MeetAnalytics {
-//    Working on Meet Analytics
-    private String[][] data; //row = {id,start,end}
+    // Working on Meet Analytics
+    private String[][] data; // row = {id,start,end}
 
-    public MeetAnalytics(String[][] data){
+    public MeetAnalytics(String[][] data) {
         this.data = data;
     }
 
-//    Active member = Number of people still Alive
-    public Map<String,String> activeUsers(){
-        Map<String,String> active = new HashMap<>();
-        for (int i = 0; i < data.length; i++){
-            if(data[i][2].equalsIgnoreCase("online")){
+    // Active member = Number of people still Alive
+    public Map<String, String> activeUsers() {
+        Map<String, String> active = new HashMap<>();
+        for (int i = 0; i < data.length; i++) {
+            if (data[i][2].equalsIgnoreCase("online")) {
                 active.put(data[i][0], data[i][1]);
             }
         }
         return active;
     }
 
-//  Left Member = Number of people who left the Meeting
+    // Left Member = Number of people who left the Meeting
     public Map<String, String[]> leftUsers() {
         Map<String, String[]> left = new HashMap<>();
 
         for (int i = 0; i < data.length; i++) {
             if (!data[i][2].equalsIgnoreCase("online")) {
-                left.put(data[i][0], new String[]{data[i][1], data[i][2]});
+                left.put(data[i][0], new String[] { data[i][1], data[i][2] });
             }
         }
         return left;
     }
 
-//    Return Total Length To Know Total user
-    public int size(){
+    // Return Total Length To Know Total user
+    public int size() {
         return data.length;
     }
 
     public static void main(String[] args) {
         String[][] cloudData = {
-                {"u1", "10:00", "11:00"},
-                {"u2", "10:30", "online"},
-                {"u3", "11:00", "11:45"}
+                { "u1", "10:00", "11:00" },
+                { "u2", "10:30", "online" },
+                { "u3", "11:00", "11:45" }
         };
         MeetAnalytics ma = new MeetAnalytics(cloudData);
 
