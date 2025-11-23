bbeab9518b014b8046195207d9fd0a34f147d6e7
enchanteddev <119434133+enchanteddev@users.noreply.github.com>
2025-11-24 01:37:41 +0530

    added sanity test

---
+++ java/module-dynamo/src/test/java/com/swe/dynamo/SanityTest.java
@@ -0,0 +1,30 @@
+package com.swe.dynamo;
+
+
+import com.swe.core.ClientNode;
+
+public class SanityTest {
+
+    public static void main(String[] args) throws Exception {
+        Dynamo dynamo = Dynamo.getInstance();
+        dynamo.registerDisconnectHandler(() -> {
+            System.err.println("We seem disconnected from the network.");
+        });
+        dynamo.addUser(new ClientNode("10.128.6.193", 1212), new ClientNode("10.128.5.156", 1212));
+        System.out.println("Connected to the network.");
+
+
+        dynamo.subscribe(1, (data) -> {
+            System.out.println("Received data: " + new String(data));
+            return null;
+        });
+
+        dynamo.sendData("Hello, world!".getBytes(), new ClientNode[] { new ClientNode("10.128.5.156", 1212) }, 1, 0);
+        System.out.println("Sent data to the network.");
+
+        Thread.sleep(10000);
+
+        dynamo.closeDynamo();
+        System.out.println("Closed the network.");
+    }
+}
